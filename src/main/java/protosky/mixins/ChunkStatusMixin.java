aa8871c0a4430ffb648b3de1283253709b7fa734
polarbub <71790726+polarbub@users.noreply.github.com>
2022-03-01 16:40:53 -0600

    Before migrating to 1.18.2

--- src/main/java/protosky/mixins/ChunkStatusMixin.java
+++ src/main/java/protosky/mixins/ChunkStatusMixin.java
@@ -5,15 +5,18 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.StructureManager;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.Heightmap;
+import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.chunk.ProtoChunk;
+import net.minecraft.world.dimension.DimensionOptions;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import protosky.gen.WorldGenUtils;
+import net.minecraft.world.dimension.DimensionType;
 
 import java.util.EnumSet;
 import java.util.List;
@@ -26,10 +29,12 @@ public abstract class ChunkStatusMixin
 {
     // LIGHT
     @Inject(method = "method_20614", at = @At("HEAD"))
-    //This is really not called 'onLighting' It's called 'GenerationTask'
+    //This is really not called 'onLighting' It's a lambda. Finding 'ChunkStatus FEATURES' should work to get to it.
     private static void onLighting(ChunkStatus targetStatus, Executor executor, ServerWorld world, ChunkGenerator generator, StructureManager structureManager, ServerLightingProvider lightingProvider, Function function, List chunks, Chunk chunk, boolean bl, CallbackInfoReturnable<CompletableFuture> cir)
     {
-        if(bl || !chunk.getStatus().isAtLeast(targetStatus)) {
+        //if (world.getRegistryKey() == World.END) System.out.println("end");
+        if((bl || !chunk.getStatus().isAtLeast(targetStatus)) && !world.getDimension().hasEnderDragonFight()) {
+        //if(bl || !chunk.getStatus().isAtLeast(targetStatus)) {
             WorldGenUtils.clearEntities((ProtoChunk)chunk, world);
             WorldGenUtils.deleteBlocks((ProtoChunk) chunk, world);
             if (new ChunkPos(world.getSpawnPos()).equals(chunk.getPos())) {
